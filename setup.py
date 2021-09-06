b3d44b9ccfc42d162b1b66670808a3aae956478a
anabudisa <ana@simula.no>
2021-09-06 18:44:12 +0200

    update license

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
       packages=setuptools.find_packages(),
       classifiers=[
          "Programming Language :: Python :: 3",
-         "License :: GNU GPLv3 License",
+         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
          "Operating System :: OS Independent",
       ],
  )
