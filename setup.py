866e823103f1ac405d0e2bbc7ae562b6e3d4b22f
anabudisa <ana@simula.no>
2021-09-06 18:26:03 +0200

    update 2

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
-#!/usr/bin/env python
+import setuptools
 
-from distutils.core import setup
-setup(name = "cbc.block",
-      version = "0.0.1",
-      description = "Block utilities",
-      author = "Joachim Berdal Haga",
-      author_email = "jobh@simula.no",
-      url = "http://www.launchpad.net/cbc.block/",
-      packages = ["block", "block.algebraic", "block.algebraic.trilinos", "block.iterative", "block.algebraic.petsc", "block.algebraic.hazmath"]
-)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+      name='cbc.block',
+      version='1.0.1',
+      scripts=['cbc.block'] ,
+      author="Joachim Berdal Haga",
+      author_email="jobh@simula.no",
+      description="Block utilities for FENiCS/dolfin",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://bitbucket.org/fenics-apps/cbc.block/",
+      packages=setuptools.find_packages(),
+      classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: GNU GPLv3 License",
+         "Operating System :: OS Independent",
+      ],
+ )
 
