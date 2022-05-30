0f92e714a415b2dce0fb55ed0f20bf5bf03bea56
Ana Budisa <ana@simula.no>
2022-05-30 18:41:54 +0200

    small update

--- block/algebraic/hazmath/precond.py
+++ block/algebraic/hazmath/precond.py
@@ -48,10 +48,12 @@ def block_mat_to_block_dCSRmat(A):
                 csr = A[i][j].mat().getValuesCSR()  # todo: eliminate zeros!
                 mat = haznics.create_matrix(csr[2], csr[1], csr[0], A[i][j].size(1))
                 Abdcsr.set(i, j, mat)
-            if not A[i][j]:
+            elif not A[i][j]:
                 mat = haznics.dCSRmat()
                 haznics.dcsr_null(mat)
                 Abdcsr.set(i, j, mat)
+            else:
+                return NotImplemented
 
     return Abdcsr
 
