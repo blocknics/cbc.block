ec9f266114d39fa06f8d22834c61a7440fab80c9
Joachim B Haga <jobh@simula.no>
2023-02-09 14:23:54 +0100

    Working, but ugly/inefficient

--- block/algebraic/petsc/solver.py
+++ block/algebraic/petsc/solver.py
@@ -1,24 +1,34 @@
 from block import block_mat, block_vec
-from block.block_base import block_base
+from block.block_base import block_base, block_container
 from block.object_pool import vec_pool
 from dolfin import GenericVector, Matrix, PETScVector
 import numpy as np
 from petsc4py import PETSc
 from .precond import petsc_base, mat as single_mat, vec as single_vec
 
+# VecNest is a perfect fit for block_vec, but I can't get it to work (gives
+# error code 86), and I don't know how to get proper error messages or traces
+# from PETSc. When set to False, it works but
+# - is very inefficient (lots of copying of vectors)
+# - doesn't work in parallel
+USE_VECNEST=False
+
 def mat(A, _sizes=None):
     if isinstance(A, block_mat):
         # We need sizes upfront in case there are matrix-free blocks, creating
-        # vectors is wasteful but easy
+        # a vector to find the size is wasteful but easy
         row_sz = [(v.local_size(), v.size()) for v in A.create_vec(dim=1)]
         col_sz = [(v.local_size(), v.size()) for v in A.create_vec(dim=0)]
-        m = PETSc.Mat().createNest([[mat(m, (row_sz[r],col_sz[c])) for (c,m) in enumerate(row)] for (r,row) in enumerate(A.blocks)])
+        m = PETSc.Mat().createNest([[mat(m, (row_sz[r],col_sz[c])) for (c,m) in enumerate(row)]
+                                    for (r,row) in enumerate(A.blocks)])
+        if USE_VECNEST:
+            m.setVecType(PETSc.Vec.Type.NEST)
         return m
     elif isinstance(A, block_base) or np.isscalar(A):
         if not _sizes:
             raise ValueError('Cannot create unsized PETSc matrix')
         Ad = PETSc.Mat().createPython(_sizes)
-        Ad.setPythonContext(wrap_mat(A))
+        Ad.setPythonContext(petsc_py_wrapper(A))
         Ad.setUp()
         return Ad
     elif isinstance(A, Matrix):
@@ -28,52 +38,66 @@ def mat(A, _sizes=None):
 
 def vec(v):
     if isinstance(v, block_vec):
-        return PETSc.Vec().createNest([vec(x) for x in v])
+        if USE_VECNEST:
+            return PETSc.Vec().createNest([single_vec(x) for x in v])
+        else:
+            vecs = [vec(x) for x in v]
+            sizes = [x.getSizes() for x in vecs]
+            vs = PETSc.Vec().create(comm=vecs[0].comm)
+            vs.setType(PETSc.Vec.Type.STANDARD)
+            vs.setSizes(sum(s[1] for s in sizes))
+            arr = vs.getArray()
+            i0 = 0
+            for vv in vecs:
+                arr2 = vv.getArray()
+                arr[i0:i0+len(arr2)] = arr2
+                i0 += len(arr2)
+            return vs
     elif isinstance(v, GenericVector):
         return single_vec(v)
     else:
         raise TypeError(str(type(v)))
 
-def Vec(v):
+def Vec(v, creator):
     if isinstance(v, PETSc.Vec):
-        if v.type == PETSc.Vec.Type.NEST:
-            return block_vec([Vec(x) for x in v])
+        if USE_VECNEST:
+            if v.type == PETSc.Vec.Type.NEST:
+                return block_vec([Vec(x) for x in v])
+            else:
+                return PETScVector(v)
         else:
-            return PETScVector(v)
+            if isinstance(creator, block_container):
+                ret = creator.create_vec(dim=1)
+                arr = v.getArray()
+                i0 = 0
+                for vv in ret:
+                    vv.set_local(arr[i0:i0+len(vv)])
+                    i0 += len(vv)
+                return ret
+            else:
+                return PETScVector(v)
     else:
         raise TypeError(str(type(v)))
 
-class wrap_mat:
+class petsc_py_wrapper:
+    # Python context for cbc.block actions (PC.Type.PYTHON, Mat.Type.PYTHON)
     def __init__(self, A):
         self.A = A
 
     def mult(self, mat, x, y):
-        new_y = vec(self.A * Vec(x))
+        new_y = vec(self.A * Vec(x, self.A))
         y.aypx(0, new_y)
-
-class wrap_precond:
-    def __init__(self, P):
-        self.P = P
-
-    def setUp(self, pc):
-        #B, P = pc.getOperators()
-        #ctx = B.getPythonContext()
-        pass
 
     def apply(self, pc, x, y):
-        # x and y are petsc vectors
-        new_y = vec(self.P * Vec(x))
-        y.aypx(0, new_y)
+        return self.mult(None, x, y)
+
+    def setUp(self, pc):
+        pass
 
 class petsc_solver(petsc_base):
     def __init__(self, A, precond, V, ksp_type, prefix, options, defaults={}):
         self.A = A
         self.Ad = mat(A)
-        print(f'# petsc matrix type: {self.Ad.getType()}')
-        if self.Ad.getType() == 'nest':
-            for i in range(self.A.blocks.shape[0]):
-                for j in range(self.A.blocks.shape[1]):
-                    print(f'#  {i,j} -> {self.Ad.getNestSubMatrix(0,0).getType()}')
 
         prefix, self.optsDB = self._merge_options(prefix=prefix, options=options, defaults=defaults)
 
@@ -86,17 +110,16 @@ class petsc_solver(petsc_base):
         if precond is not None:
             pc = self.petsc_op.getPC()
             pc.setType(PETSc.PC.Type.PYTHON)
-            pc.setPythonContext(wrap_precond(precond))
+            pc.setPythonContext(petsc_py_wrapper(precond))
 
     def __str__(self):
         return '<%s ksp of %s>'%(self.__class__.__name__, str(self.A))
 
     def matvec(self, b):
         self.petsc_op.setUp()
-        x = self.A.create_vec(dim=1)
-        print('# petsc sizes (A, b, x):', self.petsc_op.getOperators()[0].getSizes(), vec(b).getSizes(), vec(x).getSizes())
-        self.petsc_op.solve(vec(b), vec(x))
-        return x
+        x = self.Ad.createVecLeft()
+        self.petsc_op.solve(vec(b), x)
+        return Vec(x, self.A)
 
     @vec_pool
     def create_vec(self, dim=1):
--- demo/petsc.py
+++ demo/petsc.py
@@ -1,6 +1,6 @@
 from block import *
-from block.algebraic.hazmath import AMG
-from block.algebraic.petsc import KSP
+from block.algebraic.hazmath import AMG as hazAMG
+from block.algebraic.petsc import KSP, AMG
 from block.dolfin_util import BoxBoundary
 from block.iterative import Richardson
 from dolfin import *
@@ -9,7 +9,7 @@ from dolfin import *
 # Basic Poisson (poisson.py)
 #########################
 
-mesh = UnitSquareMesh(16,16)
+mesh = UnitSquareMesh(32,32)
 V = FunctionSpace(mesh, "CG", 1)
 
 f = Expression("sin(pi*x[0])", degree=2)
@@ -21,29 +21,89 @@ L = f*v*dx
 A = assemble(a)
 b = assemble(L)
 
-u = Function(V)
-solve(A, u.vector(), b)
+u = Function(V).vector()
+solve(A, u, b)
 
 #########################
 # Solve as single-matrix
 #########################
 
-Ainv = KSP(A, precond=AMG(A), ksp_type='cg')
+Apre = AMG(A)
+Ainv = KSP(A, ksp_type='cg', pc_type='ilu')
 # Alternatively:
 #Ainv = KSP(A, ksp_type='cg', pc_type='hypre', pc_hypre_type='boomeramg')
 x = Ainv*b
 
-check_expected('x', x, expected=u.vector(), show=True)
+check_expected('x', x, expected=u, show=True)
 
 #########################
-# Solve as (trivial) block-matrix
+# Solve a (trivial) block-matrix, using KSP Ainv as preconditioner
 #########################
 
 AA = block_mat([[A]])
 bb = block_vec([b])
-AAinv = KSP(AA, ksp_type='cg', pc_type='none')
+AApre = block_mat([[Ainv]])
+AAinv = KSP(AA, precond=AApre, ksp_type='cg')
 
-xx = AAinv*bb # PETSC_ERR_ARG_WRONG    62   /* wrong argument (but object probably ok) */
+xx = AAinv*bb
 
-check_expected('x', xx[0], expected=u2.vector(), show=True)
+check_expected('x', xx[0], expected=u, show=True)
 
+#########################
+# Solve a nontrivial block-matrix (mixedpoisson.py)
+#########################
+
+BDM = FunctionSpace(mesh, "BDM", 1)
+DG = FunctionSpace(mesh, "DG", 0)
+
+tau, sigma = TestFunction(BDM), TrialFunction(BDM)
+v,   u     = TestFunction(DG),  TrialFunction(DG)
+
+class BoundarySource(UserExpression):
+    def __init__(self, mesh):
+        super().__init__(self)
+        self.mesh = mesh
+    def eval_cell(self, values, x, ufc_cell):
+        cell = Cell(self.mesh, ufc_cell.index)
+        n = cell.normal(ufc_cell.local_facet)
+        g = sin(5*x[0])
+        values[0] = g*n[0]
+        values[1] = g*n[1]
+    def value_shape(self):
+        return (2,)
+
+bcs = block_bc([
+    [DirichletBC(BDM, BoundarySource(mesh), BoxBoundary(mesh).ns)],
+    None
+], symmetric=True)
+
+f = Expression("10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)", degree=4)
+
+a11 = dot(sigma, tau) * dx
+a12 = div(tau) * u * dx
+a21 = div(sigma) * v *dx
+L2  = - f * v * dx
+
+AA = block_assemble([[a11, a12],
+                     [a21,  0 ]])
+
+bb = block_assemble([0, L2])
+
+bcs.apply(AA).apply(bb)
+
+# Extract the individual submatrices
+[[A, B],
+ [C, _]] = AA
+
+Ap = hazAMG(A)
+Lp = Richardson(C*B, precond=0.5, iter=40, name='L^', show=0)
+
+AAp = block_mat([[Ap, 0],
+                 [0,  Lp]])
+
+AAinv = KSP(AA, precond=AAp, ksp_type='minres')
+Sigma, U = AAinv * bb
+
+print('expected (from mixedpoisson.py): Sigma=1.2138, U=6.7166')
+check_expected('Sigma', Sigma, show=True)
+check_expected('U', U, show=True)
